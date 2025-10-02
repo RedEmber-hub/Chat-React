74051ed4d0751687b4256b3d389883ca4abcbb52
RedEmber-hub <redember38@gmail.com>
2025-10-02 15:41:11 +0300

    в сообщениях могут быть картинки и другие файлы

--- src/components/atomics/Message/Message.tsx
+++ src/components/atomics/Message/Message.tsx
@@ -19,7 +19,12 @@ export default function Message({ messageProp }: MessageProps) {
   return (
     <div className={className}>
       <div className="message__attachment">
-        <span className="text-main">{messageProp.message}</span>
+        {/*  рендер картинки в зависимости от наличия картинки и их количества  */}
+        {messageProp.attachments &&
+          messageProp.attachments.map((file) => <img key={file.id} src={file.attachment_url} alt={file.file_name} />)}
+
+        {/* рендер текста в зависимости от наличия текста сообщения  */}
+        {messageProp.message && <span className="text-main">{messageProp.message}</span>}
       </div>
 
       <div className="message__footer">
--- src/mocks/messages.ts
+++ src/mocks/messages.ts
@@ -11,10 +11,10 @@ export const messages: MessageInterface[] = [
     status: 'check',
     attachments: [
       {
-        id: 1,
-        file_name: 'image_1',
-        attachment_url:
-          'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
+        id: 1, //key
+        file_name: 'image_1', //alt
+        //src
+        attachment_url: 'https://user76023.clients-cdnnow.ru/wp-content/uploads/2018/03/25345_1.jpg',
         mime_type: 'image/png',
       },
     ],
