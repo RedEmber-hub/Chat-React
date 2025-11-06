f0fb2802b4ef6eb668185365b9fe91eea3f75505
RedEmber-hub <redember38@gmail.com>
2025-11-06 15:48:45 +0300

    настроила конфиг и json под деплой

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chat/', // имя репозитория на GitHub
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react()],
 });
