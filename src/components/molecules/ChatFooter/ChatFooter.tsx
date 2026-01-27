1b5ab86f0b5e21a1cf29cac0e5077d311cf4740b
RedEmber-hub <redember38@gmail.com>
2026-01-27 17:36:22 +0300

    добавила тогл панели с эмоджи

--- src/components/molecules/ChatFooter/ChatFooter.tsx
+++ src/components/molecules/ChatFooter/ChatFooter.tsx
@@ -4,9 +4,12 @@ import { InputField } from '@/components/atoms/InputField';
 import './ChatFooter.scss';
 import { useState } from 'react';
 import { ChatFooterProps } from './ChatFooter.type';
+import EmojiPicker from 'emoji-picker-react';
+import { Panel } from '@/components/atoms/Panel';
 
 export default function ChatFooter({ onSend }: ChatFooterProps) {
   const [messageText, setMessageText] = useState('');
+  const [isEmojiVisible, setEmojiVisible] = useState(false);
 
   function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
     if (event.key === 'Enter') {
@@ -23,11 +26,22 @@ export default function ChatFooter({ onSend }: ChatFooterProps) {
     setMessageText('');
   }
 
+  function handleEmojiPanel() {
+    setEmojiVisible((value) => !value);
+  }
+
   return (
     <div className="chat__footer">
       <InputField size="m" color="primary">
         <InputField.Slot>
-          <Button size="s" icon="emoji-icon" />
+          <Button size="s" icon="emoji-icon" onClick={handleEmojiPanel} />
+          {isEmojiVisible && (
+            <div className="chat__wrapper">
+              <Panel>
+                <EmojiPicker onEmojiClick={(emojiObject) => console.log(emojiObject)} />
+              </Panel>
+            </div>
+          )}
         </InputField.Slot>
 
         <InputField.Field
